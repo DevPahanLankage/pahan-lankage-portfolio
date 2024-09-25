de48154470442f82a17a8f216d4d6887bf21de59
Pahan Lankage <pahan.lankage@icloud.com>
2024-09-25 23:20:02 +0530

    Added a working contact me submission form straight from the site

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+window.onload = function () {
+  // Reset the form fields when the page loads
+  document.getElementById("form").reset();
+};
